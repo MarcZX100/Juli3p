00fee95ba45130a05ab2ab00e30f2b3df495db64
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2020-09-28 18:17:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,14 +21,14 @@ fs.readdir("./events/", (err, files) => {
 });
 
  
-client.command = new Discord.Collection();
+client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	// set a new item in the Collection
 	// with the key as the command name and the value as the exported module
-	client.command.set(command.name, command);
+	client.commands.set(command.name, command);
   }
 
 
