5ade90285ac24680016c312679c2c94f191faf48
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2020-09-28 17:20:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,79 +35,15 @@ fs.readdir("./commands/", (err, files) => {
 
 
 
-client.commands = new Discord.Collection();
 
-const cooldowns = new Discord.Collection();
-
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
-}
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-	client.user.setActivity(`pp!help | Connected to ${client.guilds.cache.size} servers and ${client.users.cache.size} users!`)
+	client.user.setActivity(`Hola soy Juliiii y estoy con mis  ${client.users.cache.size} amigos UwU`)
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
  console.log(`Listo, con ${client.users.cache.size} usuarios, en ${client.channels.cache.size} canales de ${client.guilds.cache.size} servidores.`); 
-});
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-	
-	if (command.guildOnly && message.channel.type === 'dm') {
-	return message.reply('I can\'t execute that command inside DMs!');
-}
-	
-          if (command.args && !args.length) {
-        	let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-            if (!cooldowns.has(command.name)) {
-	           cooldowns.set(command.name, new Discord.Collection());
-                }
-
-            const now = Date.now();
-            const timestamps = cooldowns.get(command.name);
-            const cooldownAmount = (command.cooldown || 3) * 1000;
-
-      if (timestamps.has(message.author.id)) {
-	     const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-	     if (now < expirationTime) {
-		    const timeLeft = (expirationTime - now) / 1000;
-	       	return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
-				     
-	      timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-}}
-	
-try {
-	command.execute(message, args);
-} catch (error) {
-	console.error(error);
-	message.reply('there was an error trying to execute that command!');
-}
 });
 
 
