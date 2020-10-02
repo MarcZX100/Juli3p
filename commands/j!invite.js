e2f62fbf0e75f30749f5a9fa9a8cf9259a6cb7ee
MarcZX10 <67475268+MarcZX100@users.noreply.github.com>
2020-10-02 08:29:55 +0200

    Update j!invite.js

--- commands/j!invite.js
+++ commands/j!invite.js
@@ -9,7 +9,6 @@ let inline = true
 message.channel.send({embed: {
       color: 3447003,
       fields: [{
-        {
           name: "Invitame a tu servidor :DD",
           value: `[**--Obama**](https://discord.com/api/oauth2/authorize?client_id=760113701419221032&permissions=8&scope=bot)`, inline
         }
